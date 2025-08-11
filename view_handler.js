2afbb16f1ba813e18c4f5b52c933be0939782dfc
caspar <casparmonckton236@gmail.com>
2025-08-12 09:12:33 +1000

    Implemented lazy loading in the gallery to address the long loading times that occured when accessing it

--- view_handler.js
+++ view_handler.js
@@ -1,10 +1,9 @@
+
 function display_tapestry(canvas, pixel_grid) {
 	let ctx = canvas.getContext("2d");
 	let cell_width = Math.floor(canvas.width / pixel_grid[0].length);
 	let cell_height = Math.floor(canvas.height / pixel_grid.length);
 	
-	console.log(cell_width);
-	console.log(cell_height);
 	
 	for (const [y, pixel_row] of pixel_grid.entries()) {
 		for (const [x, pixel] of pixel_row.entries()) {
@@ -12,4 +11,33 @@ function display_tapestry(canvas, pixel_grid) {
 			ctx.fillRect(x * cell_width, y * cell_height, cell_width, cell_height);
 		}
 	}
+}
+
+// Code courtesy of Chisom Kanu on https://blog.openreplay.com/lazy-loading-in-javascript/
+function isElementInViewport(element) {
+	const rect = element.getBoundingClientRect();
+	return rect.bottom > 0 && rect.top < window.innerHeight; 
+}
+
+
+
+function lazyLoadContent() {
+	
+	const lazyContentElements = document.querySelectorAll(".lazy-content");
+ 
+	lazyContentElements.forEach((element) => {
+		if (isElementInViewport(element) && element.getAttribute("loaded") === "false") {
+			const rule = JSON.parse(element.getAttribute("rule"));
+			const tape = JSON.parse(element.getAttribute("tape"));
+			display_tapestry(element, generate_tapestry_data(new Rule(rule, env.num_states), new Tape(4, tape), tape.length));
+			element.setAttribute("loaded", "true");
+		}
+	});
+	
+	const remainingUnloaded = document.querySelectorAll(".lazy-content[loaded='false']");
+	console.log(remainingUnloaded)
+    if (remainingUnloaded.length === 0) {
+        window.removeEventListener("scroll", lazyLoadContent);
+        console.log("All elements loaded, scroll listener removed");
+    }
 }
